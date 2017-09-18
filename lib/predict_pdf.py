239adcdb98a1177062ff874a869d28c5fa817227
paolo_sarti <paolo.sarti@ext.injenia.it>
2017-09-18 13:27:02 +0000

    load models from predict_pdf

--- lib/predict_pdf.py
+++ lib/predict_pdf.py
@@ -1,10 +1,14 @@
 # This Python file uses the following encoding: utf-8
 from embedding import embed_document, reduce_dictionary
 from keras.preprocessing import sequence
+from keras.models import model_from_json
 from collections import OrderedDict
+from gensim.models import Doc2Vec
 from words import tokenize_doc
+from functools import partial
 import text_extraction as te
 import pandas as pd
+import json
 
 def predict_documents_str(filenames, txts, gensim_model, keras_model, permitted_words):
     filtered_filenames = [f for f,t in zip(filenames, txts) if (t != None and len(t)>0)]
@@ -25,7 +29,24 @@ def predict_documents_txt(filenames, gensim_model, keras_model, permitted_words)
     txts = [open(filename).read() for filename in filenames]
     return predict_documents_str(filenames, txts, gensim_model, keras_model, permitted_words)
 
-def predictions_dataframe(pdf_names, filtered_filenames, predictions, csv_out_file, labels_map):
+def load_prediction_models(gensim_file='models/gensim_model_5000.d2v', 
+                           keras_model_file='models/keras_model.json',
+                           keras_weights_file='models/keras_weights_5000.h5',
+                           permitted_words_file='first_5000_words.json'):
+    models = {}
+    models['gensim_model'] = Doc2Vec.load(gensim_file)
+
+    with open(keras_model_file) as f:
+        models['keras_model'] = model_from_json(f.read())
+    models['keras_model'].load_weights(keras_weights_file)
+
+    with open(permitted_words_file) as f:
+        models['permitted_words'] = set(json.load(f))
+
+    return partial(predict_documents_pdf, **models)
+                           
+
+def predictions_dataframe(pdf_names, filtered_filenames, predictions, csv_out_file, labels_map=['NON COSTITUTIVO', 'COSTITUTIVO']):
     filt_filenames_set = set(filtered_filenames)
     labels = [labels_map[int(round(pred))] for pred in predictions]
     not_predicted_pdfs = [pdf for pdf in pdf_names if pdf not in filt_filenames_set]
