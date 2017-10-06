b0db3e3d4d0acdbfd03e40104839757c63876a40
paolo_sarti <paolo.sarti@ext.injenia.it>
2017-10-06 15:01:55 +0000

    new sentencing

--- lib/words.py
+++ lib/words.py
@@ -27,6 +27,10 @@ def clean_string(s):
     us = to_utf8(s)
     return compress_blanks(u''.join(c if c.lower() in allowed_lower_chars_punct else ' ' for c in us))
 
+def clean_string_not_compressed(s):
+    us = to_utf8(s)
+    return u''.join(c if c.lower() in allowed_lower_chars_punct else ' ' for c in us)
+
 def splitted_words(txt):
     return re.sub('[^\w]',' ',txt).split()
 
@@ -75,6 +79,14 @@ def replace_evil_dots_and_underscores(txt, rep=r''):
     no_underscores = re.sub(u'_', ''+rep, no_maiusc)
     return no_underscores
 
+def replace_evil_dots_and_underscores_newline(txt, rep=r''):
+    no_abbr = re.sub(r'([bcdfghjklmnpqrstvwxyz])\.(?!\n)', r'\1'+rep, txt)
+    no_nums = re.sub(r'(\d)\.(?!\n)', r'\1'+rep, no_abbr)
+    no_nums = re.sub(r'\.(\d)', r'\1'+rep, no_nums)
+    no_maiusc = re.sub(r'([A-Z])\.(?!\n)', r'\1'+rep, no_nums)
+    no_underscores = re.sub(r'_', ''+rep, no_maiusc)
+    return no_underscores
+
 def rm_stop_words(words):
     return [word for word in words if not (word in stop_words)]
 
@@ -90,9 +102,14 @@ def not_so_naive_split(txt, digit_replacement='NUM', split='.', min_words = 2):
     sentences_rep = (list(map(replace_num_old, s)) for s in sentences)
     return [rm_stop_words(s) for s in sentences_rep if len(rm_stop_words(s))>=min_words]
 
-def sentences_doc(doc, rep=r''):
-    txt = clean_string(doc)
-    clean_txt = replace_evil_dots_and_underscores(txt, rep=rep)
+def sentences_doc(doc, rep=r'', newline=False):
+    if newline:
+        txt = clean_string_not_compressed(doc)
+        clean_txt = replace_evil_dots_and_underscores_newline(txt, rep=rep)
+        clean_txt = compress_blanks(clean_txt)
+    else:
+        txt = clean_string(doc)
+        clean_txt = replace_evil_dots_and_underscores(txt, rep=rep)
     return split_sents(clean_txt) 
 
 def tokenize_sentences(sents, min_words=2, replace_nums=True, rm_stop_words=True):
@@ -108,8 +125,8 @@ def tokenize_sentences(sents, min_words=2, replace_nums=True, rm_stop_words=True
             splitted_sents.append(sent)
     return splitted_sents
 
-def tokenize_doc(doc, min_words=2, replace_nums=True, rm_stop_words=True):
-    sents = sentences_doc(doc)
+def tokenize_doc(doc, min_words=2, replace_nums=True, rm_stop_words=True, rep=r'', newline=False):
+    sents = sentences_doc(doc, rep, newline)
     return tokenize_sentences(sents, min_words, replace_nums, rm_stop_words)
 
 def filter_indexes(pred, l):
