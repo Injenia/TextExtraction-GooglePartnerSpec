d5e1f75a612a264f7c8c1bb2633d3f6e4babefbd
paolo_sarti <paolo.sarti@ext.injenia.it>
2017-10-19 15:39:04 +0000

    Training on new dataset

--- lib/pretty_testing.py
+++ lib/pretty_testing.py
@@ -1,6 +1,63 @@
 # This Python file uses the following encoding: utf-8
 from __future__ import division
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
+from keras.callbacks import EarlyStopping
+
+def fitValidationSplit(model, X_train, y_train, split=2/7, epochs=1000, patience=10):
+    model.fit(X_train, y_train, validation_split=split, verbose=2, epochs=epochs, callbacks=[EarlyStopping(monitor='loss', patience=100)]) #categorical_accuracy
+
+def fitValidate(model, X_train, y_train, X_val, y_val, labels,  model_filename, class_weights, patience=10, resume=False, max_train_accuracy=0.99):
+    best_accuracy = -1
+    patience_count = 0
+    i = 0
+    if resume:
+        model.load_weights(model_filename)
+    while True:
+        hist = model.fit(X_train, y_train, verbose=2, epochs=1, class_weight=class_weights)
+        loss = hist.history['loss'][0]
+        train_accuracy = hist.history['categorical_accuracy'][0]
+        y_pred = model.predict_classes(X_val)
+
+        # Compute scores on validation
+        accuracy = accuracy_score(y_val, y_pred)
+        precision = precision_score(y_val, y_pred, average='weighted')
+        recall = recall_score(y_val, y_pred, average='weighted')
+        f1 = f1_score(y_val, y_pred, average='weighted')
+
+        report = classification_report(y_val, y_pred)
+        print('\n\nReport at epoch '+str(i))
+        print('Loss: ' + str(loss))
+        print('Train accuracy: '+ str(train_accuracy))
+        print()
+
+        print('Validation accuracy: '+str(accuracy))
+        print('Validation precision: '+str(precision))
+        print('Validation recall: '+str(recall))
+        print('Validation F1 Score: '+str(f1))
+        print()
+        print(report)
+
+        cm = confusion_matrix(y_val, y_pred)
+        print('Confusion matrix')
+        print_cm(cm, labels)
+        if best_accuracy == -1 or accuracy > best_accuracy:
+            best_accuracy = accuracy
+            #Save the model with the best accuracy on the validation set
+            model.save_weights(model_filename)
+            patience_count = 0
+        else:
+            patience_count += 1
+            print('\nPatience count: '+str(patience_count)+'/'+str(patience))
+            if patience_count == patience:
+                print('Accuracy on validation stopped decreasing') #loss
+                break
+        i += 1
+        if train_accuracy >= max_train_accuracy:
+            print('Maximum train accuracy reached...')
+            break
+    model.load_weights(model_filename)
+
+
 
 def print_cm(cm, labels, hide_zeroes=False, hide_diagonal=False, hide_threshold=None):
     """pretty print for confusion matrixes"""
--- train_lstm.py
+++ train_lstm.py
@@ -4,9 +4,10 @@ from keras.models import model_from_json
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
+from keras.optimizers import Adam, RMSprop
 from keras.preprocessing import sequence
 from sklearn.model_selection import train_test_split
-
+from keras.callbacks import EarlyStopping
 from lib.pretty_testing import predict_test
 
 import numpy as np
@@ -16,8 +17,9 @@ import pickle
 np.random.seed(8)
 
 dataset_file = 'embedded_docs_with_verb.p'
-model_weights_file = 'models/keras_new_weights_with_verb.h5'
-
+model_weights_file = 'models/keras_new_weights_with_verb_es.h5'
+lr = 0.0001
+epochs = 100
 
 # load prepared data
 with open(dataset_file) as f:
@@ -35,17 +37,21 @@ print 'Data padded'
 X_train, X_test, y_train, y_test = train_test_split(padded_data, labels, train_size=0.9, stratify=labels)
 del padded_data
 
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, train_size=0.95, stratify=y_train)
+
 print 'Data splitted'
 
 model = Sequential()
 model.add(LSTM(100, input_shape = (200, 100)))
 #model.add(Dropout(0.2))
 model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer=RMSprop(lr=lr), metrics=['accuracy'])
 print(model.summary())
 
 print 'Start training'
-model.fit(X_train, y_train, epochs=3, batch_size=64)
+model.fit(X_train, y_train, epochs=epochs, batch_size=64, validation_data=(X_val, y_val),
+          callbacks=[EarlyStopping(monitor='val_acc', patience=5)])
+
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
