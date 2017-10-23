50d0dff8fa92173c49ccc39c74888609fcf5d29d
Luca Paganelli <luca.paganelli@injenia.it>
2017-10-23 13:02:37 +0000

    DEMO-2

---
+++ demo/2017-10-24/lib/predict_pdf.py
@@ -0,0 +1,67 @@
+# This Python file uses the following encoding: utf-8
+from embedding import embed_document, reduce_dictionary
+from keras.preprocessing import sequence
+from keras.models import model_from_json
+from collections import OrderedDict
+from gensim.models import Doc2Vec
+from words import tokenize_doc
+from functools import partial
+import text_extraction as te
+import pandas as pd
+import json
+
+def predict_documents_str(filenames, txts, gensim_model, keras_model, permitted_words):
+    filtered_filenames = [f for f,t in zip(filenames, txts) if (t != None and len(t)>0)]
+    not_empty_txts =  [t for t in txts if  (t != None and len(t)>0)]
+    if len(filtered_filenames) == 0:
+        return [], []
+    
+    splitted_txts = (tokenize_doc(txt) for txt in not_empty_txts) 
+    filtered_txts = (list(reduce_dictionary(document, permitted_words)) for document in splitted_txts)
+    embedded_txts = [embed_document(gensim_model, doc, permitted_words) for doc in filtered_txts]
+    padded_data = sequence.pad_sequences(embedded_txts, maxlen=200, padding="pre", truncating="post", value=0.0, dtype='float32')
+    probs = keras_model.predict_proba(padded_data, verbose=0)
+    return [prob[0] for prob in probs], filtered_filenames
+
+def predict_documents_pdf(filenames, gensim_model, keras_model, permitted_words, do_ocr=False):
+    txts = [te.extract_text(filename, do_ocr) for filename in filenames]
+    return predict_documents_str(filenames, txts, gensim_model, keras_model, permitted_words)
+
+def predict_documents_txt(filenames, gensim_model, keras_model, permitted_words):
+    txts = [open(filename).read() for filename in filenames]
+    return predict_documents_str(filenames, txts, gensim_model, keras_model, permitted_words)
+
+def load_prediction_models(gensim_file='models/gensim_model_5000.d2v', 
+                           keras_model_file='models/keras_model.json',
+                           keras_weights_file='models/keras_weights_5000.h5',
+                           permitted_words_file='first_5000_words.json'):
+    models = {}
+    models['gensim_model'] = Doc2Vec.load(gensim_file)
+
+    with open(keras_model_file) as f:
+        models['keras_model'] = model_from_json(f.read())
+    models['keras_model'].load_weights(keras_weights_file)
+
+    with open(permitted_words_file) as f:
+        models['permitted_words'] = set(json.load(f))
+
+    return partial(predict_documents_pdf, **models)
+                           
+
+def predictions_dataframe(pdf_names, filtered_filenames, predictions, csv_out_file, labels_map=['NON COSTITUTIVO', 'COSTITUTIVO']):
+    filt_filenames_set = set(filtered_filenames)
+    labels = [labels_map[int(round(pred))] for pred in predictions]
+    not_predicted_pdfs = [pdf for pdf in pdf_names if pdf not in filt_filenames_set]
+    err_fill = [u'']*len(not_predicted_pdfs)
+    pred_fill = [u'']*len(filtered_filenames)
+    err_msgs = [u'scansione']*len(not_predicted_pdfs)
+    
+    df_dict = OrderedDict([('Nome file', filtered_filenames+not_predicted_pdfs),
+                          ('Errore', ['No']*len(filtered_filenames) + ['Si']*len(not_predicted_pdfs)),
+                          ('Messaggio errore', pred_fill + err_msgs),
+                          ('Output rete', predictions + err_fill),
+                          ('Predizione', labels + err_fill),])
+    
+    df = pd.DataFrame(df_dict)
+    
+    return df
---
+++ demo/2017-10-24/lib/text_extraction.py
@@ -0,0 +1,69 @@
+# This Python file uses the following encoding: utf-8
+from lib.words import splitted_words
+
+from google.cloud import vision
+from google.cloud.vision import types
+
+from subprocess import call
+from utils import rm_dir
+
+import textract
+import codecs
+import os
+
+def convert_to_txt(img):
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
+    
+    with open(img, 'rb') as image_file:
+        content = image_file.read()
+        
+    image = types.Image(content=content)
+    # Performs label detection on the image file
+    response = client.text_detection(image=image)
+    labels = response.text_annotations
+    try:
+        return (labels[0].description)
+    except:
+        return ""   
+
+def extract_text(f, do_ocr=False, png_dir='../tmp', min_words=150, pages=5):
+    try:
+        text = textract.process(f)
+        #with codecs.open("vision-api-result.txt", "w", encoding='utf-8') as o:
+        #    o.write(text)
+            
+        if len(splitted_words(text)) <= min_words:
+            #print f, u"è una scansione"
+            if not do_ocr:
+                return ''
+            print f, "Estrazione testo con Vision API."
+            rm_dir(png_dir)
+            os.mkdir(png_dir)
+            # out-10.png
+            call(("convert -verbose -density 300 %s -quality 100 %s" 
+                  % (f, os.path.join(png_dir,os.path.basename(f)[:-3]+'png'))).split(' '))
+            # os.listdir is not ordered by name, this fixes it
+            images = sorted(os.listdir(png_dir), key=lambda item: (int(item.partition('-')[2].partition('.')[0])))
+                        
+            if len(images) > 0:
+                res = ""
+                for i,el in enumerate(images):
+                    if i < pages:
+                        txt = convert_to_txt(os.path.join(png_dir,el))
+                        res += txt
+                    else:
+                        break
+                rm_dir(png_dir)        
+                if res != "":
+                    return res
+                else:
+                    print f, "is empty"
+                    return ''
+        else:
+            return text
+    except Exception as e:
+        print e
+        #print f, "e' un documento illeggibile..." 
+        rm_dir(png_dir)
+        return ''
